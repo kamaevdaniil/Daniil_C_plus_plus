e694aa2a797c1f3f634f1730d257191dac6aa205
Пользователь <user@school.local>
2021-12-28 17:59:46 +0300

    comment

---
+++ test_1.cpp
@@ -0,0 +1,13 @@
+﻿// ConsoleApplication51.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+// Примечание: Эта функция не является методом класса!
+void some_function() {
+	cout << "Тестовая загрузка файла";
+}
+int main() {
+	setlocale(0, "");
+	some_function();
+}
